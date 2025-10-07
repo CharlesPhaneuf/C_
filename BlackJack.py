226b1b0a45e52dba0cce70123fbf310908051b30
CharlesPhaneuf <phaneufc@seminaire-sherbrooke.qc.ca>
2025-10-07 18:53:47 +0000

    s

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,18 @@
 import random
 
-choices = [2, 3, 4, 5, 6, 7, 8, , 9, 10, "Jack", "Quene", "King", "Ace"]
+# Define the card values
+card_choice = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": (1, 11)}
 
-while True:
-    x = random.choice(choices)
-    print(x)
+card_names = list(card_choice.keys())
+
+
+random_cards = random.choices(card_names, k=2)
+
+sum_of_values = 0
+for card in random_cards:
+    if card == "Ace":
+        sum_of_values += 11  
+    else:
+        sum_of_values += card_choice[card]
+        
+print("Sum of Values:", sum_of_values)
